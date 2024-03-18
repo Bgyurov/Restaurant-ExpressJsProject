02ead56be8970c435d4d35dc9a510c1a26651499
Bgyurov <49366221+Bgyurov@users.noreply.github.com>
2024-03-18 16:56:54 +0200

    Implement review/comments section

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,14 +1,24 @@
 const Review = require('../models/Review')
 const Meal = require('../models/Meal')
+const User = require('../models/User.js')
 
 exports.getHomePage = async (req,res)=>{
-    let comments = await Review.find().lean()
+
    
-    res.render('home',{comments})
+    res.render('home')
 }
-exports.getAboutPage =  (req,res)=>{
-    
-    res.render('about')
+exports.getAboutPage = async (req,res)=>{
+    let comments = await Review.find().lean()
+    for (let comment of comments) {
+        // Find the user who made the review
+        let user = await User.findById(comment.owner).lean();
+        if (user) {
+            // Add user profile image to the comment object
+            comment.userProfileImage = user.profileImage; // Assuming profileImage is the field containing profile picture
+        }
+    }
+
+    res.render('about' , {comments})
 }
 exports.getGallaryPage =  (req,res)=>{
     
