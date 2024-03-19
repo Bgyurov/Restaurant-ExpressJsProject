139262cb719bcb1acbaaf298da3994a9ea2c9a1e
Bgyurov <49366221+Bgyurov@users.noreply.github.com>
2024-03-19 08:49:53 +0200

    Refactor profile page
    And fix navbar photo high

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -2,10 +2,11 @@ const authService = require('../service/authService')
 const Reservation = require('../models/Reservation')
 exports.getProfile = async(req,res)=>{
     const user = await authService.getUserbyUsername(req.user.username)
+    let profileImage = user.profileImage
     let userEmail = user.email
     let userUsername = user.username
     let owner = req.params.profileId
-    const books = await Reservation.find({owner}).lean()
+    const reservations = await Reservation.find({owner}).lean()
     
-    res.render('profile',{userUsername,userEmail,books})
+    res.render('profile',{userUsername,userEmail,profileImage,reservations})
 }
