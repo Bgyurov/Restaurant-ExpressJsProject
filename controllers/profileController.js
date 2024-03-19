c76eba9372493824905125b0988aa7686569193f
Bgyurov <49366221+Bgyurov@users.noreply.github.com>
2024-03-19 09:52:59 +0200

    Refactor profile view and book table view

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -1,12 +1,13 @@
 const authService = require('../service/authService')
 const Reservation = require('../models/Reservation')
 exports.getProfile = async(req,res)=>{
+    let moreReservations = false
     const user = await authService.getUserbyUsername(req.user.username)
     let profileImage = user.profileImage
     let userEmail = user.email
     let userUsername = user.username
     let owner = req.params.profileId
-    const reservations = await Reservation.find({owner}).lean()
+    let reservations = await Reservation.find({owner}).lean()
     
     res.render('profile',{userUsername,userEmail,profileImage,reservations})
 }
